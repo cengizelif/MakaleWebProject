1e803670f479e7ad8a79753354df7f3f38e14b74
cenelif@gmail.com <cenelif@gmail.com>
2022-05-31 20:51:56 +0300

    Kategori ekle/sil

--- Makale.BusinessLayer/KategoriYonet.cs
+++ Makale.BusinessLayer/KategoriYonet.cs
@@ -20,5 +20,78 @@ namespace Makale.BusinessLayer
         {
             return repo_kat.Find(x => x.Id == id);
         }
+
+        BusinessLayerResult<Kategori> kat_sonuc = new BusinessLayerResult<Kategori>();
+
+        public BusinessLayerResult<Kategori> KategoriKaydet(Kategori model)
+        {           
+            Kategori kat = repo_kat.Find(x => x.Baslik == model.Baslik);
+
+            if (kat != null)
+            {              
+                  kat_sonuc.hata.Add("Kategori adı kayıtlı");               
+            }
+            else
+            {
+                int sonuc = repo_kat.Insert(new Kategori()
+                {
+                     Baslik=model.Baslik,
+                     Aciklama=model.Aciklama
+                });
+            }
+
+            return kat_sonuc;
+
+        }
+
+
+        public BusinessLayerResult<Kategori> KategoriUpdate(Kategori model)
+        {
+            Kategori kat = repo_kat.Find(x=>x.Baslik==model.Baslik && x.Id!=model.Id);
+
+            if(kat!=null)
+            {
+                kat_sonuc.hata.Add("Kategori adı kayıtlı");
+            }
+            else
+            {
+                kat_sonuc.Sonuc = repo_kat.Find(x => x.Id == model.Id);
+                kat_sonuc.Sonuc.Baslik = model.Baslik;
+                kat_sonuc.Sonuc.Aciklama = model.Aciklama;
+
+               int sonuc=repo_kat.Update(kat_sonuc.Sonuc);
+
+                if(sonuc>0)
+                {
+                    kat_sonuc.Sonuc = repo_kat.Find(x => x.Id == model.Id);
+                }
+            }
+
+            return kat_sonuc;
+        }
+    
+    
+
+        public BusinessLayerResult<Kategori> KategoriSil(int id)
+        {
+            Kategori k = repo_kat.Find(x => x.Id == id);
+
+            //kategorinin notlarını bul
+            //notların yorumlarını bul sil
+            //notların like bul sil
+            //notu sil 
+            //kategori sil
+
+            if(k==null)
+            {
+                kat_sonuc.hata.Add("Kategori bulunamadı.");
+            }
+
+       
+            int sonuc=repo_kat.Delete(k);
+
+            return kat_sonuc;
+        }
+    
     }
 }
